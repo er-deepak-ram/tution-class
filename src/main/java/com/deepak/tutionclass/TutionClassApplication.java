bd1e8bc811c925d3ac8c17a96e09b43796e9dad7
Deepak Kumar Ram <deepak.kumar2305@gmail.com>
2021-07-02 10:01:32 +0530

    Initial commit

---
+++ src/main/java/com/deepak/tutionclass/TutionClassApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.tutionclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutionClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutionClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepak/tutionclass/TutionClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.tutionclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutionClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
